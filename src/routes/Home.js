2243bed3b5c3ba91a214de133f3a07b9596c44af
Ko-GyeongTae <kokt0203@naver.com>
2021-12-06 08:43:10 +0900

    ADD - kakao map

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Cookies } from 'react-cookie';
 import 'semantic-ui-css/semantic.min.css';
 
 const cookies = new Cookies();
+const { kakao } = window;
+
+const Location = () => {
+    useEffect(() => {
+        var container = document.getElementById('map');
+        var options = {
+            center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+            level: 3
+        };
+
+        var map = new kakao.maps.Map(container, options);
+        var markerPosition = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
+        var marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+        marker.setMap(map);
+
+    }, [])
+
+
+    return (
+        <div>
+            <div id="map" style={{ width: "100vw", height: "70vh" }}></div>
+        </div>
+    )
+}
 
 function Login() {
-    
+
     const logout = () => {
         cookies.remove("accessToken");
         window.location.reload(false);
@@ -24,14 +50,13 @@ function Login() {
                         </div>
                     </div>
 
-                    <div class="ui text container">
+                    <div class="ui text container" style={{marginBottom: 30}}>
                         <h1 class="ui inverted header">
                             Track On
                         </h1>
                         <h2>You can track your Arduino</h2>
-                        
                     </div>
-
+                    <Location/>
                 </div>
             </div>
         </>

6819d15a6f40954c8a1f07bde9aa3e4bbc6121c1
Ko-GyeongTae <kokt0203@naver.com>
2021-12-06 10:13:00 +0900

    ADD

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,12 +4,22 @@ import 'semantic-ui-css/semantic.min.css';
 
 const cookies = new Cookies();
 const { kakao } = window;
+const position = [
+    {
+        "lat": 33.450701,
+        "long": 126.570667
+    },
+    {
+        "lat": 33.45,
+        "long": 126.57
+    }
+]
 
 const Location = () => {
     useEffect(() => {
         var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
             mapOption = {
-                center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                center: new kakao.maps.LatLng(position[0].lat, position[0].long), // 지도의 중심좌표
                 level: 3 // 지도의 확대 레벨
             };
 
@@ -27,30 +37,32 @@ const Location = () => {
         var zoomControl = new kakao.maps.ZoomControl();
         map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-        // 마커가 표시될 위치입니다 
-        var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+        position.map(p => {
+            
+            // 마커가 표시될 위치입니다 
+            var markerPosition = new kakao.maps.LatLng(p.lat, p.long);
 
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-            position: markerPosition
-        });
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                position: markerPosition
+            });
 
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(map);
 
+            var iwContent = '<div style="padding:5px; color:black">Welcome to Trackon!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                iwPosition = new kakao.maps.LatLng(p.lat, p.long); //인포윈도우 표시 위치입니다
 
-        var iwContent = '<div style="padding:5px; color:black">Welcome to Trackon!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-            iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+            // 인포윈도우를 생성합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                position: iwPosition,
+                content: iwContent
+            });
 
-        // 인포윈도우를 생성합니다
-        var infowindow = new kakao.maps.InfoWindow({
-            position: iwPosition,
-            content: iwContent
-        });
-
-        // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-        infowindow.open(map, marker);
-
+            // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+            infowindow.open(map, marker);
+            return p;
+        })
     }, [])
 
 
@@ -74,7 +86,7 @@ function Login() {
 
                     <div class="ui container">
                         <div class="ui large secondary inverted pointing menu">
-                            <a class="active item">Home</a>
+                            <a href="/" class="active item">Home</a>
                             <div class="right item">
                                 <button class="ui inverted button" onClick={logout}>LogOut</button>
                             </div>
